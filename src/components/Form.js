8726748f148c4348bed22c6f54692612ef2a9559
Himanshu Singh <himanshusingh1k@gmail.com>
2025-12-12 21:51:16 +0530

    update

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,9 +4,7 @@ const Form = () => {
   const [fieldError, setFieldsError] = useState("All fields are mandatory");
   const [nameError, setNameError] = useState("");
   const [emailError, setEmailError] = useState("");
-  const [genderError, setGenderError] = useState(
-    "Please identify as male, female or others"
-  );
+  const [genderError, setGenderError] = useState("");
   const [numError, setNumError] = useState("");
   const [pwdError, setPwdError] = useState("");
   const [greet, setGreet] = useState("");
