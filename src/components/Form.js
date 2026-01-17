74d43064790b5f97f73e5803433effae07beb656
Himanshu Singh <himanshusingh1k@gmail.com>
2026-01-17 12:01:28 +0530

    commit

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 const Form = () => {
   const [fieldError, setFieldsError] = useState("All fields are mandatory");
